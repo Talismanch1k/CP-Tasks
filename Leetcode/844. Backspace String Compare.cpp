064eb0aae9850382eb1bd4cd2bdd59294c6fd2ff
Talismanch1k <talismanch1k@yandex.ru>
2022-06-30 22:37:05 +0300

    844. Backspace String Compare.cpp

---
+++ Leetcode/844. Backspace String Compare.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string a = "", b = "";
+        for(int i = 0; i<s.size(); i++) {
+            if(s[i]=='#'){
+                if(a.size()>0) a.pop_back();
+            } else a.push_back(s[i]);
+            
+        }
+        for(int i = 0; i<t.size(); i++) {
+            if(t[i]=='#'){
+                if(b.size()>0) b.pop_back();
+            } else b.push_back(t[i]);
+            
+        }
+        if(a==b) return true;
+        else return false;
+    }
+};
