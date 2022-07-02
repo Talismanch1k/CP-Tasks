7f0776b292610b33526e0f9dea95614d8e987c9a
Talismanch1k <talismanch1k@yandex.ru>
2022-07-03 00:13:12 +0300

    Problem's descriptions added

--- Leetcode/844. Backspace String Compare.cpp
+++ Leetcode/844. Backspace String Compare.cpp
@@ -1,3 +1,11 @@
+/*
+844. Backspace String Compare
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+*/
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
